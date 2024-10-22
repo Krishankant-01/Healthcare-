b25c33ecfe1b7dc58966cdf64e86f0feb65ad797
KRISHAN KANT <krishankant1705@gmail.com>
2024-10-22 11:16:40 +0530

    building the code

---
+++ dbConnection.js
@@ -0,0 +1,13 @@
+// mongoose module require maane package install krna pdega
+const mongoose=require("mongoose");
+const connectDb=async()=>{
+    try{
+        const connect = await mongoose.connect(process.env.CONNECTION_STRING)
+        console.log("Connected to mongo")
+        
+
+    }catch(error){
+        console.log(error)
+    }
+}
+module.exports=connectDb;
